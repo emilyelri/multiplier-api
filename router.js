f825549a486d093d7060567a161786e0de3f07df
emilyelri <richard.emily.e@gmail.com>
2021-03-28 18:52:10 -0400

    added brief documentation to /GET request

--- router.js
+++ router.js
@@ -3,7 +3,30 @@ const router = require("express").Router();
 // GET DOCUMENTATION
 router.get("/", (req, res) => {
   try {
-    res.json({ Documentation: "documentation" });
+    res.json({
+      Documentation: {
+        Purpose: "API will calculate and return the product of two numbers.",
+        Instructions: {
+          POST:
+            "Send a POST request to /api/ with the two numeric arguments of any variable type (integer, float, string, etc.) in JSON format.",
+          Note:
+            "Both parameters MUST be numbers or API will return error status 500.",
+          Parameters: {
+            multiplier: "{number}",
+            multiplicand: "{number}",
+            Example_Parameters: {
+              multiplier: 3,
+              multiplicand: 4,
+            },
+          },
+          Result: {
+            Product:
+              "API will return the product of the two factors in JSON format.",
+            Example_Result: { PRODUCT: 12 },
+          },
+        },
+      },
+    });
   } catch {
     res.status(500).json({ message: "Failed to accesss API documentation." });
   }
@@ -15,7 +38,7 @@ router.post("/", validateRequest, (req, res) => {
   const product = multiplier * multiplicand;
 
   try {
-    res.status(201).json({ PRODUCT: product });
+    res.status(200).json({ PRODUCT: product });
   } catch {
     res.status(500).json({ ERROR: "API failed to return product." });
   }
@@ -24,24 +47,29 @@ router.post("/", validateRequest, (req, res) => {
 function validateRequest(req, res, next) {
   const { multiplier, multiplicand } = req.body;
 
-  if (!multiplier || !multiplicand) {
+  if (!multiplier) {
     return res
-      .status(404)
-      .send({ ERROR: "Must provide exactly two integers to multiply." });
+      .status(500)
+      .send({ ERROR: "Must provide multiplier." });
   }
+
+  if (!multiplicand) {
+    return res
+      .status(500)
+      .send({ ERROR: "Must provide multiplicand." });
+  }
+
 
   if (isNaN(multiplier)) {
     return res
-      .status(404)
+      .status(500)
       .send({ ERROR: "Multiplier is not a number - please provide a number." });
   }
 
   if (isNaN(multiplicand)) {
-    return res
-      .status(404)
-      .send({
-        ERROR: "Multiplicand is not a number - please provide a number.",
-      });
+    return res.status(500).send({
+      ERROR: "Multiplicand is not a number - please provide a number.",
+    });
   }
 
   next();
